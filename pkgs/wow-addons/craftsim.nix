a9d048679ef49759f2bb95812dcb048b6dc41fb9
Karina J. Kwiatek <kjk@kjk.dog>
2026-09-05 02:38:03 +0200

    Fix args for CraftSim

--- pkgs/wow-addons/craftsim.nix
+++ pkgs/wow-addons/craftsim.nix
@@ -3,8 +3,8 @@ githubReleaseAddon {
   name = "CraftSim";
   owner = "derfloh205";
   repo = "CraftSim";
-  rev = "v27.0.2";
+  rev = "27.0.2";
   asset = "CraftSim-27.0.2.zip";
-  hash = "02sbnfdllvz2a8sc4k0hwgbywhfy3vdx7hlcl5hhry3bh8wrz0im";
+  sha256 = "02sbnfdllvz2a8sc4k0hwgbywhfy3vdx7hlcl5hhry3bh8wrz0im";
   subdir = "CraftSim";
 }
