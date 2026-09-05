49a6e0df7729350a6d37a6de068a81e4f453bb57
Karina J. Kwiatek <kjk@kjk.dog>
2026-09-05 02:09:07 +0200

    Fix hash for littlewigs

--- pkgs/wow-addons/littlewigs.nix
+++ pkgs/wow-addons/littlewigs.nix
@@ -3,5 +3,5 @@ githubAddon {
   owner = "BigWigsMods";
   repo = "LittleWigs";
   rev = "v12.0.63";
-  hash = "1xx0290v9nyyn5nv9qsnr8n393kwn8q1qbz75gxx2n4kjxpnhm9l";
+  hash = "0073grldhkqqnz796vqwy7rl89cpp8a9rvy35m782p08y1837nh2";
 }
