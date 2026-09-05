02a0dc7658962da235757b182df6fbf1db4d2555
raccube <6197148+raccube@users.noreply.github.com>
2026-09-05 07:29:04 +0000

    Update WoW addon pins

--- pkgs/wow-addons/gtfo.nix
+++ pkgs/wow-addons/gtfo.nix
@@ -1,7 +1,7 @@
 {zipAddon, ...}:
 zipAddon {
   name = "gtfo";
-  version = "6.9.1";
-  url = "https://www.wowinterface.com/downloads/getfile.php?id=17996&aid=171263";
-  sha256 = "1bhqmbs2psbm3kpaanlw5manq63sppnxkrzmk7pj797k2jr18bd8";
+  version = "6.10";
+  url = "https://cdn.wowinterface.com/downloads/getfile.php?id=17996&d=1788542139&minion";
+  sha256 = "11gwiji1pvd0zaqblh44mnqvvjgif1z3vk0mjdc7709s0pnkn58g";
 }
