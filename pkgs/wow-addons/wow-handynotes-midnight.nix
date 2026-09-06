80adca6c55e21d83e0139d9d8d4134278d7657a1
raccube <6197148+raccube@users.noreply.github.com>
2026-09-06 07:42:35 +0000

    Update WoW addon pins

--- pkgs/wow-addons/wow-handynotes-midnight.nix
+++ pkgs/wow-addons/wow-handynotes-midnight.nix
@@ -3,8 +3,8 @@ githubReleaseAddon {
   name = "HandyNotes_MidnightTreasures";
   owner = "kemayo";
   repo = "wow-handynotes-midnight";
-  rev = "v65";
-  asset = "HandyNotes_MidnightTreasures-v65.zip";
-  sha256 = "0mwnhw3037ha18qz4y0xb66xhjw77h4fip62nh9nvs31wsgz979b";
+  rev = "v66";
+  asset = "HandyNotes_MidnightTreasures-v66.zip";
+  sha256 = "06il2s5rqqz718fywm5qvsifz008xvifijaxhlmj6scdy4dqaxmx";
   subdir = "HandyNotes_MidnightTreasures";
 }
